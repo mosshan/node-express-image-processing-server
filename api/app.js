b4136c7ebc3d1f1b6e3b5a62ccc12449170f6d1c
mosshan <mosshan@yahoo.com>
2022-05-31 13:34:06 -0400

    Edited Submit Button

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html' );
+
 app.use('/*', (request, response)=>{
   response.sendFile(pathToIndex);
 });
